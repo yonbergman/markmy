6a9816ce9ccc9fcf4820509966c2a8c53c99f894
yon <Lighthawky@gmail.com>
2014-03-14 17:46:57 +0200

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Markmy::Application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations" }
   resources :blurbs, :path => :words, only: [:show, :create, :destroy]
-  get '/words' => 'application#home', :as => ['home', :new_blurb]
+  get '/words' => 'application#home', :as => 'home'
+  get '/blurbs/new' => 'application#home', as: :new_blurb
   get 'my_words' => 'blurbs#index', :as => 'my_blurbs'
   root to: redirect('/words')
 
