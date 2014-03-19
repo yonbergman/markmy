c92d09ca59e7ffd24e201d736a1c1932016f10fd
Gilad Goldberg <giladgo@gmail.com>
2014-03-19 17:11:46 +0200

    Adding gravatar for blurb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
   def description
     @description || I18n.translate('site.description')
   end
+
+  def avatar_url(user)
+    default_url = "#{root_url}images/guest.png"
+    if user.guest_account?
+      default_url
+    else
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+    end
+
+  end
 end
